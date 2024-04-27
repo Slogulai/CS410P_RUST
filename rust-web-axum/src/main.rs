c0cc69bce77581a68af7016d09bcdbcf052f2338
Slogulai <153771171+Slogulai@users.noreply.github.com>
2024-04-27 13:51:30 -0700

    Added route handling, need to finish handler.rs before it will work

--- rust-web-axum/src/main.rs
+++ rust-web-axum/src/main.rs
@@ -1,6 +1,7 @@
 mod handler;
 mod response;
 mod question;
+mod route;
 
 #[allow(unused)]
 use handler::*;
@@ -8,10 +9,14 @@ use handler::*;
 use response::*;
 #[allow(unused)]
 use question::*;
+#[allow(unused)]
+use route::*;
 
 
 #[allow(unused)]
 use axum::{
+    header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE},
+    HeaderValue, Method,
     error_handling::HandleError,
     // Json, Router,
     extract::{Path, State},
@@ -31,6 +36,10 @@ use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
+
+use route::create_router;
+use tower_http::cors::Corslayer;
+
 //~~~~~ASYNC STUFF~~~~~~
 
 //https://codevoweb.com/create-a-simple-api-in-rust-using-the-axum-framework/
@@ -48,11 +57,17 @@ async fn health_check() -> impl IntoResponse {
 #[allow(unused)]
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(health_check));
+    //let app = Router::new().route("/", get(health_check));
+    let cors = Corslayer::new()
+        .allow_any_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
+        .allow_credentials(true)
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
+
+    let app = create_router().layer(cors);
 
     println!("Starting server on port 3000...");
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
---
+++ rust-web-axum/src/route.rs
@@ -0,0 +1,30 @@
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use crate::{
+    handler::{
+        create_question_handler, 
+        delete_question_handler, 
+        edit_question_handler,
+        get_question_handler,
+        health_check,
+        question_list_handler,
+    },
+    question,
+    *,
+};
+pub fn create_router() -> Router {
+    let db = question::question_db();
+    Router::new()
+        .route("api/healthchecker", get(health_check))
+        .route("/api/questions", 
+            post(create_question_handler).get(question_list_handler),
+        )
+        .route("/api/questions/:id", 
+            get(get_question_handler)
+            .patch(edit_question_handler)
+            .delete(delete_question_handler),
+        )
+        .with_state(db)
+}
